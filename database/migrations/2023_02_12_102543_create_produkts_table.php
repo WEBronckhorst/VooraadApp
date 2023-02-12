184ccf269fcba7cf9999987e7399ed8fbf9d48dc
WEBronckhorst <info@webronckhorst.nl>
2023-02-12 13:01:53 +0100

    updat 0.1.2

--- database/migrations/2023_02_12_102543_create_produkts_table.php
+++ database/migrations/2023_02_12_102543_create_produkts_table.php
@@ -15,8 +15,8 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('code');
-            $table->string('description');
-          
+            $table->string('description')->nullable();
+
             $table->timestamps();
         });
     }
